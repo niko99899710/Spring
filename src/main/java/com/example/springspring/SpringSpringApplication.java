3fefa6276dbc58a2d62c938db8ebae07a77067c8
Karina <bagdasarova-k@inbox.ru>
2023-06-26 21:27:59 +0500

    Spring service

---
+++ src/main/java/com/example/springspring/SpringSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.springspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springspring/SpringSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
